ab2391045c0081f0996ae7a32fd1d34a20dda330
Mathias Kahl <mathias.kahl@gmail.com>
2021-04-06 20:32:08 +0200

    modal animation

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -10,20 +10,27 @@ interface Props {
 
 const Modal = ({visible, children, onRequestClose}: Props) => {
   const opacity = useRef(new Animated.Value(0.0)).current;
+  const slideIn = useRef(new Animated.Value(50.0)).current;
 
   useEffect(() => {
     if (visible) {
-      const anim = Animated.timing(opacity, {
+      const fadeInAnim = Animated.timing(opacity, {
         duration: 250,
         toValue: 1,
-        easing: Easing.ease,
+        easing: Easing.out(Easing.cubic),
         useNativeDriver: true,
       });
-      anim.start();
+      const slideInAnim = Animated.timing(slideIn, {
+        duration: 250,
+        toValue: 0,
+        easing: Easing.out(Easing.cubic),
+        useNativeDriver: true,
+      });
+      Animated.stagger(150, [fadeInAnim, slideInAnim]).start();
 
-      return () => anim.stop();
+      return () => fadeInAnim.stop();
     }
-  }, [visible, opacity]);
+  }, [visible, opacity, slideIn]);
 
   if (!visible) {
     return null;
@@ -33,7 +40,7 @@ const Modal = ({visible, children, onRequestClose}: Props) => {
     Animated.timing(opacity, {
       duration: 250,
       toValue: 0,
-      easing: Easing.ease,
+      easing: Easing.out(Easing.cubic),
       useNativeDriver: true,
     }).start(() => onRequestClose?.());
   };
@@ -42,7 +49,10 @@ const Modal = ({visible, children, onRequestClose}: Props) => {
     <Animated.View
       style={[styles.root, {opacity: opacity}]}
       onTouchEnd={onTouchEnd}>
-      {children}
+      <Animated.View
+        style={[styles.content, {transform: [{translateY: slideIn}]}]}>
+        {children}
+      </Animated.View>
     </Animated.View>
   );
 };
@@ -56,6 +66,9 @@ const styles = StyleSheet.create({
     zIndex: 2,
     width: '100%',
     height: '100%',
+  },
+  content: {
+    flex: 1,
   },
 });
 
